c78c07122a6909b2af05756bc6f788fbb91010df
Gledson-Perdival-Junior <gledsonpmjunior@gmail.com>
2025-10-28 20:01:09 -0300

    ajuste dos elementos do grafico

--- src/pages/OrganizationManagement/OrganizationManagement.vue
+++ src/pages/OrganizationManagement/OrganizationManagement.vue
@@ -92,22 +92,22 @@ export default {
       chartLabels: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
       chartDatasets: [
         {
-          label: 'Renda',
-        data: [5000, 6000, 5500, 7000, 6500, 7200, 6800, 7100, 6900, 7500, 8000, 8200],
+          label: 'Valor em caixa',
+        data: [1000, 2000, 2900, 3900, 4600, 5000, 5000, 5700, 6200, 5500, 6100, 5900],
         borderColor: '#4B53C6',
         backgroundColor: 'rgba(75,83,198,0.2)',
         tension: 0.3 
         },
         {
         label: 'Despesas',
-        data: [3000, 3500, 3200, 4000, 3600, 3800, 3400, 3900, 3700, 4100, 4300, 4500],
+        data: [2000, 2500, 2300, 3000, 2900, 3400, 3400, 3200, 3200, 4100, 3700, 4000],
         borderColor: '#d12a2aff',
         backgroundColor: 'rgba(248,152,152,0.2)',
         tension: 0.3
-        },
-        {
-        label: 'Lucro',
-        data: [2000, 2500, 2300, 3000, 2900, 3400, 3400, 3200, 3200, 3400, 3700, 3700],
+      },
+      {
+        label: 'Entrada',
+        data: [3000, 3500, 3200, 4000, 3600, 3800, 3400, 3900, 3700, 3400, 4300, 3800],
         borderColor: '#137267ff',
         backgroundColor: 'rgba(117,212,201,0.2)',
         tension: 0.3
