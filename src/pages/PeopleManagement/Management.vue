45bd30d35ddbf8eab2d6cea1fd68f57b6a261288
fernandogentilp <gentilcx1@gmail.com>
2025-11-23 23:04:29 -0300

    Atualizando o vue de Management

--- src/pages/PeopleManagement/Management.vue
+++ src/pages/PeopleManagement/Management.vue
@@ -1,31 +1,59 @@
-<template>
-  <div class="management-container">
-    <h1>People Management</h1>
-
-    <form @submit.prevent="handleSubmit">
-      <div class="form-group">
-        <label for="name">Nome:</label>
-        <input id="name" v-model="form.name" type="text" placeholder="Digite o nome" required />
-      </div>
-
-      <div class="form-group">
-        <label for="role">Cargo:</label>
-        <input id="role" v-model="form.role" type="text" placeholder="Digite o cargo" required />
-      </div>
-
-      <div class="form-group">
-        <label for="email">E-mail:</label>
-        <input id="email" v-model="form.email" type="email" placeholder="Digite o e-mail" required />
-      </div>
-
-      <button type="submit">Cadastrar</button>
-    </form>
-
-    <div v-if="submitted" class="success-message">
-      ✅ Pessoa cadastrada com sucesso!
-    </div>
-  </div>
+<template lang="pug">
+div.cadastro-page
+   div.cadastro-form
+      span.page-title Formulário Semanal
+      el-date-picker(
+         placeholder="Data do Sábado"
+         v-model="cadastroData.semana"
+      )
+      el-input(
+         placeholder="Segunda"
+         v-model="cadastroData.segunda"
+      )
+      el-input(
+         placeholder="Terça"
+         v-model="cadastroData.terca"
+      )
+      el-input(
+         placeholder="Quarta"
+         v-model="cadastroData.quarta"
+      )
+      el-input(
+         placeholder="Quinta"
+         v-model="cadastroData.quinta"
+      )
+      el-input(
+         placeholder="Sexta"
+         v-model="cadastroData.sexta"
+      )
+      el-input(
+         placeholder="Sabado"
+         v-model="cadastroData.sabado"
+      )
+      el-input(
+         placeholder="Domingo"
+         v-model="cadastroData.domingo"
+      )
+      el-button(
+         type="primary"
+         round
+         @click="save"
+      ) Enviar
 </template>
 
-<script src="./Management.js"></script>
-<style lang="scss" src="./Management.scss"></style>
+<script>
+    import scriptModule from './Management.js'
+
+    export default{
+        name: scriptModule.name,
+        components: scriptModule.components,
+        mounted: scriptModule.mounted,
+        data: scriptModule.data,
+        computed: scriptModule.computed,
+        methods: scriptModule.methods
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import './Management.scss';
+</style>
