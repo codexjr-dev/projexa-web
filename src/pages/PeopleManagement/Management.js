b10e18a019822de9762e5592f2417e0e39298629
fernandogentilp <gentilcx1@gmail.com>
2025-11-23 23:03:28 -0300

    Atualizando o js de Management

--- src/pages/PeopleManagement/Management.js
+++ src/pages/PeopleManagement/Management.js
@@ -1,26 +1,67 @@
+import { mapActions } from 'vuex'
+import { ElNotification, ElMessageBox } from 'element-plus'
+
+
+// TODO
+/**
+ * Modificar os avisos de cada método, cada retorno.
+ */
 export default {
-  name: "PeopleManagement",
+   name: 'forms',
 
-  data() {
-    return {
-      form: {
-        name: "",
-        role: "",
-        email: ""
+   data() {
+      return {
+         cadastroData: {
+            name: '',
+            presidentData: {
+               name: '',
+               email: '',
+               birthDate: '',
+               password: '',
+            },
+         },
+      }
+   },
+
+   mounted() {
+      this.$store.commit('SHOW_SIDEBAR', false);
+   },
+
+   methods: {
+      ...mapActions({
+         createEj: 'createEj',
+      }),
+
+      async save() {
+         ElNotification.closeAll();
+         ElNotification({
+            title: 'Aguarde...',
+            message: 'O cadastro da empresa pode levar alguns instantes',
+            type: 'warning',
+         });
+
+         try {
+            await this.createEj(this.cadastroData)
+            ElNotification.closeAll();
+            ElNotification({
+               title: 'Sucesso',
+               message: 'A Empresa Júnior foi cadastrada com sucesso!',
+               type: 'success',
+            })
+            this.$router.push({ name: 'Login' })
+         } catch (error) {
+            ElNotification.closeAll();
+            ElNotification({
+               title: 'Ops!',
+               message: 'Ocorreu um erro no seu cadastro',
+               type: 'error',
+            })
+         }
+
       },
-      submitted: false
-    };
-  },
 
-  methods: {
-    handleSubmit() {
-      this.submitted = true;
-
-      console.log("Dados cadastrados:", this.form);
-
-      this.form.name = "";
-      this.form.role = "";
-      this.form.email = "";
-    }
-  }
-};
+      login() {
+         this.$router.push({ name: 'Login' })
+      }
+   }
+}
